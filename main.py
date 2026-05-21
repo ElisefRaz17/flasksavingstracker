99bdf04214cd71539d67a143d6c2e52ee341fb79
Elise Frazier <elisefraz11@gmail.com>
2026-05-21 15:50:15 -0400

    fixing API error

--- main.py
+++ main.py
@@ -31,8 +31,11 @@
 #     app.run(debug=True, host="0.0.0.0", port=5001)
     
 from __init__ import create_app
+from flask_cors import CORS
 
 app = create_app()
+CORS(app, resources={r"/*": {"origins": "https://localhost:4200"}})
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5001)
