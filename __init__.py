c0fda27fc39347fa983e01c4d4f30a026de91ac6
Elise Frazier <elisefraz11@gmail.com>
2026-05-21 15:54:07 -0400

    fixing API error

--- __init__.py
+++ __init__.py
@@ -1,7 +1,10 @@
 from flask import Flask
+from flask_cors import CORS
 from api.routes import api_bp
 
 def create_app():
     app = Flask(__name__)
+    CORS(app, resources={r"/api/*": {"origins": "https://localhost:4200"}})
+
     app.register_blueprint(api_bp, url_prefix='/api')
     return app
