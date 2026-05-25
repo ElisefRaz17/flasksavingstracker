129ea24e2a6a96221c0fd8a1d249ad718a358f4c
Elise Frazier <elisefraz11@gmail.com>
2026-05-25 10:35:13 -0400

    disabled auto refresh token

--- api/database.py
+++ api/database.py
@@ -1,10 +1,11 @@
 import os
-from supabase import create_client, Client
+from supabase import create_client, Client, ClientOptions
 from dotenv import load_dotenv
 
 load_dotenv()
 
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+options = ClientOptions(auto_refresh_token=False)
 
-supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY,options)
